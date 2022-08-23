44e8be8cdd6b7ef67a0ec502e9d642615a00ddba
zsrjim <55557022+zsrjim@users.noreply.github.com>
2022-08-23 21:22:28 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -41,7 +41,7 @@ docker(){
 }
 #7一键安装docker-compose
 docker-compose(){
-	curl -L "https://github.com/docker/compose/releases/download/v2.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+	curl -L "https://github.com/docker/compose/releases/download/v2.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose && docker-compose --version
 }
 echo && echo -e " 
   
